03d4270e80203298f8ec1a956e99de918f813e94
hengchih <chrischang@91mai.com>
2015-01-02 16:58:37 +0800

    m

--- javascripts/nineyiTouch.js
+++ javascripts/nineyiTouch.js
@@ -7,29 +7,58 @@
 
 
         var nt = (function (p) {
-            var sx = 0, sy = 0, cx = 0, cy = 0, isup=true;
+            var isMove = false,
+                reference,
+                referenceX = 0,
+                referenceY = 0,
+                offsetX = 0,
+                offsetY = 0,
+                behavior,
+                rd = 180 / Math.PI;
             return {
                 touchstart: function(e){
-                    var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
-                    sx = touch.pageX - cx;
-                    sy = touch.pageY - cy;
-                    isup = false;
+                    isMove = false;
+                    reference = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+                    referenceX = reference.pageX;
+                    referenceY = reference.pageY;
                 },
                 touchmove: function(e){
-                    var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
-                    isup || $(this).css({
-                        'webkitTransform':'translate3d('+(touch.pageX-sx)+'px,0px,0)',
-                        'mozTransform':'translate3d('+(touch.pageX-sx)+'px,0px,0)',
-                        'msTransform':'translate3d('+(touch.pageX-sx)+'px,0px,0)',
-                        'transform':'translate3d('+(touch.pageX-sx)+'px,0px,0)'
-                    });
-                    p.swipeLeft && p.swipeLeft();
+                    var $this = $(this);
+                    var newReference = (e.originalEvent.touches[0] || e.originalEvent.changedTouches[0]),
+                        newReferenceX = newReference.pageX,
+                        newReferenceY = newReference.pageY,
+                        offsetX = newReferenceX - referenceX,
+                        offsetY = newReferenceY - referenceY;
+
+                    isMove = true;
+                    reference = newReference;
+                    referenceX = newReferenceX;
+                    referenceY = newReferenceY;
+
+                    //console.log(Math.abs( offsetY / offsetX ) * rd);
+                    if( Math.abs( offsetY / offsetX ) * rd < 60 ){ //水平
+                        reference  = newReference;
+                        //获取css3属性值
+                        var transform = $this.css('-webkit-transform') || $container.css('transform') || $container.css('-moz-transform') || $container.css('-ms-transform');
+                        var ml = +(transform.match(/\-?[0-9]+\.?[0-9]*/g)[4]);
+                        console.log(offsetX);
+                        //offset = ml + prevOrNext;
+                        /*超过界限禁止移动*/
+                        //if( offset > 0 || offset <= -$containerWidth + targetWidth + space ){return;}
+                        /*超过界效果*/
+                        //if(offset < -$containerWidth + targetWidth + space || offset > 0){offset =  ml + prevOrNext / 3 ;}
+
+                        //开启硬件加速
+                        $(this).css('-webkit-transform','translate3d('+ (ml + offsetX) +'px, 0, 0)');
+                        $(this).css('transform','translate3d('+ (ml + offsetX) +'px, 0, 0)');
+                    }
                 },
                 touchend: function(e){
-                    var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
-                    cx = touch.pageX - sx;
-                    cy = touch.pageY - sy;
-                    isup=true;
+                    if(!isMove){ //點擊
+                        alert('1');
+                    }else{ //移動
+
+                    }
                 }
             };
         })(p);
